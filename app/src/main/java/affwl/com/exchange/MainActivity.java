17a2d44fd813ee14d91d26c904a6bb86b4ea73a6
anandv112 <anandv112@gmail.com>
2018-03-27 20:00:53 +0530

    teenpatti class and layout and backbutton popup icons on teenpatti layout with tejas's popup integrations

--- app/src/main/java/affwl/com/exchange/MainActivity.java
+++ app/src/main/java/affwl/com/exchange/MainActivity.java
@@ -1,18 +1,118 @@
 package affwl.com.exchange;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView showPopupBtn, closeRateus, closeHelpBtn, closeTrophyBtn;
+    PopupWindow RateuspopupWindow, HelpUspopupWindow, TrophypopupWindow;
+    RelativeLayout RelativeLayoutloader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Popup for RateUS
+        showPopupBtn = (ImageView) findViewById(R.id.rateus_btn_loader);
+        RelativeLayoutloader = (RelativeLayout) findViewById(R.id.linearLayoutloader);
+
+        showPopupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //instantiate the popup.xml layout file
+                LayoutInflater layoutInflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View customView = layoutInflater.inflate(R.layout.rateus_popup,null);
+
+                closeRateus = (ImageView) customView.findViewById(R.id.close_rateus);
+
+                //instantiate popup window
+                RateuspopupWindow = new PopupWindow(customView, RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+                //display the popup window
+                RateuspopupWindow.showAtLocation(RelativeLayoutloader, Gravity.CENTER, 0, 0);
+
+                //close the popup window on button click
+                closeRateus.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        RateuspopupWindow.dismiss();
+                    }
+                });
+
+            }
+        });
+
+        // Popup for Help
+        showPopupBtn = (ImageView) findViewById(R.id.help_btn_loader);
+        RelativeLayoutloader = (RelativeLayout) findViewById(R.id.linearLayoutloader);
+
+        showPopupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //instantiate the popup.xml layout file
+                LayoutInflater layoutInflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View customView = layoutInflater.inflate(R.layout.help_popup,null);
+
+                closeHelpBtn = (ImageView) customView.findViewById(R.id.close_helpus);
+
+                //instantiate popup window
+                HelpUspopupWindow = new PopupWindow(customView, RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+                //display the popup window
+                HelpUspopupWindow.showAtLocation(RelativeLayoutloader, Gravity.CENTER, 0, 0);
+
+                //close the popup window on button click
+                closeHelpBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        HelpUspopupWindow.dismiss();
+                    }
+                });
+
+            }
+        });
+
+        // Popup for Leaderboard
+        showPopupBtn = (ImageView) findViewById(R.id.trophy_btn_loader);
+        RelativeLayoutloader = (RelativeLayout) findViewById(R.id.linearLayoutloader);
+
+        showPopupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //instantiate the popup.xml layout file
+                LayoutInflater layoutInflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View customView = layoutInflater.inflate(R.layout.rateus_popup,null);
+
+                closeTrophyBtn = (ImageView) customView.findViewById(R.id.close_rateus);
+
+                //instantiate popup window
+                TrophypopupWindow = new PopupWindow(customView, RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+                //display the popup window
+                TrophypopupWindow.showAtLocation(RelativeLayoutloader, Gravity.CENTER, 0, 0);
+
+                //close the popup window on button click
+                closeTrophyBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        TrophypopupWindow.dismiss();
+                    }
+                });
+            }
+        });
+
 
 
         // Animation of chips on main page
