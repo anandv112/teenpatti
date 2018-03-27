a36080d45b3cd5c0ea22572ea8c6be74df4393ad
tejas027 <tejas027@github.com>
2018-03-27 10:32:45 +0530

    RateUS Popup Added

--- app/src/main/java/affwl/com/exchange/MainActivity.java
+++ app/src/main/java/affwl/com/exchange/MainActivity.java
@@ -1,13 +1,114 @@
 package affwl.com.exchange;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView showPopupBtn, closePopupBtn;
+    PopupWindow popupWindow;
+    RelativeLayout linearLayoutloader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Popup for RateUS
+        showPopupBtn = (ImageView) findViewById(R.id.rateus_btn_loader);
+        linearLayoutloader = (RelativeLayout) findViewById(R.id.linearLayoutrateus);
+
+        showPopupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //instantiate the popup.xml layout file
+                LayoutInflater layoutInflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View customView = layoutInflater.inflate(R.layout.rateus_popup,null);
+
+                closePopupBtn = (ImageView) customView.findViewById(R.id.close_rateus);
+
+                //instantiate popup window
+                popupWindow = new PopupWindow(customView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+                //display the popup window
+                popupWindow.showAtLocation(linearLayoutloader, Gravity.CENTER, 0, 0);
+
+                //close the popup window on button click
+                closePopupBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }
+                });
+
+            }
+        });
+
+        // Popup for Help
+        showPopupBtn = (ImageView) findViewById(R.id.help_btn_loader);
+        linearLayoutloader = (RelativeLayout) findViewById(R.id.linearLayoutrateus);
+
+        showPopupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //instantiate the popup.xml layout file
+                LayoutInflater layoutInflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View customView = layoutInflater.inflate(R.layout.rateus_popup,null);
+
+                closePopupBtn = (ImageView) customView.findViewById(R.id.close_rateus);
+
+                //instantiate popup window
+                popupWindow = new PopupWindow(customView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+                //display the popup window
+                popupWindow.showAtLocation(linearLayoutloader, Gravity.CENTER, 0, 0);
+
+                //close the popup window on button click
+                closePopupBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }
+                });
+
+            }
+        });
+
+        // Popup for Leaderboard
+        showPopupBtn = (ImageView) findViewById(R.id.trophy_btn_loader);
+        linearLayoutloader = (RelativeLayout) findViewById(R.id.linearLayoutrateus);
+
+        showPopupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //instantiate the popup.xml layout file
+                LayoutInflater layoutInflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View customView = layoutInflater.inflate(R.layout.rateus_popup,null);
+
+                closePopupBtn = (ImageView) customView.findViewById(R.id.close_rateus);
+
+                //instantiate popup window
+                popupWindow = new PopupWindow(customView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+                //display the popup window
+                popupWindow.showAtLocation(linearLayoutloader, Gravity.CENTER, 0, 0);
+
+                //close the popup window on button click
+                closePopupBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }
+                });
+            }
+        });
     }
 }
